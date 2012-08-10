d8c5081171fdda104dfff270bf50c84f6ab6b732
Steve <steve@ossii.com.tw>
2012-08-10 17:28:26 +0800

    pkg_sync.sh can sync fedora 17's arm and armhfp directory

--- pkg_sync/pkg_sync.sh
+++ pkg_sync/pkg_sync.sh
@@ -5,17 +5,18 @@ git_ip=127.0.0.1
 rsync_log_path=/var/log/rsync
 pkg_sync_dir=$(pwd)
 sl_pkgs_path=/var/yum/scientific/6.2
-fc_pkgs_path=/var/yum/fedora/updates/17
+fc_pkgs_path=/var/yum/Fedora/updates/17
 meego_pkgs_path=/var/yum/meego/updates/1.2.0/repos
 sl_mirror_site=rsync://ftp.riken.jp/scientificlinux/6.2
 fc_mirror_site=rsync://mirror.nexicom.net/Fedora/updates/17
+fc_arm_mirror_site=rsync://archive.eu.kernel.org/fedora-secondary/updates/17
 meego_mirror_site=rsync://mirrors.kernel.org/mirrors/meego/updates/1.2.0/repos
 sl_git_path=/var/git/packages/scientific/6.2
-fc_git_path=/var/git/packages/fedora/17
+fc_git_path=/var/git/packages/Fedora/17
 meego_git_path=/var/git/packages/meego/1.2.0
 count=10
 
-function sl_srpm_download() {
+function sl_pkgs_download() {
   i=1
   for arch in i386 x86_64 SRPMS
   do
@@ -37,7 +38,7 @@ function sl_srpm_download() {
   grep src.rpm$ $rsync_log_path/sl_updatelist_$(date +%Y%m%d) | sort -t "/" -u -V -k 3 > $pkg_sync_dir/list/sl_git.list
 }
 
-function fc_srpm_download() {
+function fc_pkgs_download() {
   i=1
   for arch in i386 x86_64 SRPMS
   do
@@ -56,10 +57,29 @@ function fc_srpm_download() {
         fi
     done
   done
+
+  i=1
+  for arch in arm armhfp
+  do
+    while [ "$i" -le "$count" ]
+    do
+      rsync -rptgoLDv --timeout=120 --include-from=$pkg_sync_dir/list/fc_srpm.list --log-file=$rsync_log_path/fc_rsync_$(date +%Y%m%d).log \
+--progress $fc_arm_mirror_site/$arch $fc_pkgs_path >> $rsync_log_path/fc_updatelist_$(date +%Y%m%d)
+        if [ "$?" == "0" ]; then
+          echo "Download successfully(Fedora,$arch)"
+          break
+        else
+          echo "Download failed(Fedora,$arch)"
+          logger -i -p local5.notice -t pkg_rsync "Download successfully(Fedora,$arch)"
+          sleep 10
+          i=$(($i+1))
+        fi
+    done
+  done
   grep src.rpm$ $rsync_log_path/fc_updatelist_$(date +%Y%m%d) | sort -u -V > $pkg_sync_dir/list/fc_git.list
 }
 
-function meego_srpm_download() {
+function meego_pkgs_download() {
   i=1
   while [ "$i" -le "$count" ]
   do
@@ -186,7 +206,7 @@ function upload_to_fc_git(){
         mkdir ossii || exit
         cp -a SPECS/* ossii || exit
         git init
-        git add . && git commit -m "upload $pkgname-$pkgver from fedora 17" 
+        git add . && git commit -m "upload $pkgname-$pkgver from Fedora 17" 
         git remote add origin $user@$git_ip:$fc_git_path/$pkgname.git
         git push origin master
         git push origin master:refs/heads/develop
@@ -269,13 +289,17 @@ function upload_to_meego_git(){
     done
 }
 
-function update_repo() {
+function update_sl_repo() {
   createrepo --update /var/yum/scientific/6.2/i386/updates/security
   createrepo --update /var/yum/scientific/6.2/x86_64/updates/security
   createrepo --update /var/yum/scientific/6.2/SRPMS
-  createrepo --update /var/yum/fedora/updates/17/i386
-  createrepo --update /var/yum/fedora/updates/17/x86_64
-  createrepo --update /var/yum/fedora/updates/17/SRPMS/
+}
+function update_fc_repo() {
+  createrepo --update /var/yum/Fedora/updates/17/i386
+  createrepo --update /var/yum/Fedora/updates/17/x86_64
+  createrepo --update /var/yum/Fedora/updates/17/SRPMS/
+}
+function update_meego_repo() {
   createrepo --update /var/yum/meego/updates/1.2.0/repos/non-oss/ia32/packages
   createrepo --update /var/yum/meego/updates/1.2.0/repos/oss/ia32/packages
   createrepo --update /var/yum/meego/updates/1.2.0/repos/oss/source
@@ -299,16 +323,48 @@ rm $rsync_log_path/sl_updatelist_$(date +%Y%m%d) 2> /dev/null
 rm $rsync_log_path/fc_updatelist_$(date +%Y%m%d) 2> /dev/null
 rm $rsync_log_path/meego_updatelist_$(date +%Y%m%d) 2> /dev/null
 
-sl_srpm_download;
-fc_srpm_download;
-meego_srpm_download;
-send_mail;
+case "$1" in
+  "all")
+    sl_pkgs_download;
+    fc_pkgs_download;
+    meego_pkgs_download;
+    send_mail;
+    check_rpmmacros;
+    upload_to_sl_git;
+    cd ~ && rm -rf $HOME/rpmbuild/*
+    upload_to_fc_git;
+    cd ~ && rm -rf $HOME/rpmbuild/*
+    upload_to_meego_git;
+    cd ~ && rm -rf $HOME/rpmbuild/*
+    update_sl_repo;
+    update_fc_repo;
+    update_meego_repo;
+    ;;
+  "sl")
+    sl_pkgs_download;
+    send_mail;
+    check_rpmmacros;
+    upload_to_sl_git;
+    cd ~ && rm -rf $HOME/rpmbuild/*
+    update_sl_repo;
+    ;;
+  "fc")
+    fc_pkgs_download;
+    send_mail;
+    check_rpmmacros;
+    upload_to_fc_git;
+    cd ~ && rm -rf $HOME/rpmbuild/*
+    update_fc_repo;
+    ;;
+  "meego")
+    meego_pkgs_download;
+    send_mail;
+    check_rpmmacros;
+    upload_to_meego_git;
+    cd ~ && rm -rf $HOME/rpmbuild/*
+    update_meego_repo;
+    ;;
+  *)
+    ;;
+esac
 
-check_rpmmacros;
-upload_to_sl_git;
-cd ~ && rm -rf $HOME/rpmbuild/*
-upload_to_fc_git;
-cd ~ && rm -rf $HOME/rpmbuild/*
-upload_to_meego_git;
-cd ~ && rm -rf $HOME/rpmbuild/*
-update_repo;
